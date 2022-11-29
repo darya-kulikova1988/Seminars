a92c6a02fb727a4528600ebc50833b6973305d4c
Darya <darya.kulikova88@yandex.ru>
2022-11-29 16:55:56 +0300

    task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на
+//  вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int C = int.Parse(Console.ReadLine());
+
+int max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+
+Console.WriteLine(max);
