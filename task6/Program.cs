65584999f55ae3b8d83df200599d8ebb423f7e02
Darya <darya.kulikova88@yandex.ru>
2022-11-29 17:13:47 +0300

    task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int result = A%2;
+// Console.WriteLine(result);
+if(result==0) 
+{Console.WriteLine("да");}
+else{Console.WriteLine("нет");}
+
+
