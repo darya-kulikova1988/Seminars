b3a7d5db4f6139fa36458f8f23415e4fd164b95a
Darya <darya.kulikova88@yandex.ru>
2022-11-29 12:58:02 +0300

    task5 and task 7

---
+++ task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+Console.WriteLine("Введите трехзначное число: ");
+int ABC = int.Parse(Console.ReadLine());
+int result = ABC%10;
+// int A = int.Parse(Console.ReadLine());
+// int B = int.Parse(Console.ReadLine());
+// int C = int.Parse(Console.ReadLine());
+Console.WriteLine(result);
