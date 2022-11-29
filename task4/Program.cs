208c30d9342d98764f6852962391d79023c1d5d4
Darya <darya.kulikova88@yandex.ru>
2022-11-29 16:58:59 +0300

    task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -5,15 +5,15 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Введите первое число: ");
+Console.Write("Введите первое число: ");
 int A = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
+Console.Write("Введите второе число: ");
 int B = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
+Console.Write("Введите второе число: ");
 int C = int.Parse(Console.ReadLine());
 
 int max = A;
 if (B > max) max = B;
 if (C > max) max = C;
 
-Console.WriteLine(max);
+Console.WriteLine("Максимальное число: " + max);
