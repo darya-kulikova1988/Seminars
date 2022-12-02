1bd0aa4c679dada78aa1296e728b82dbe81c130e
Darya <darya.kulikova88@yandex.ru>
2022-12-02 16:55:44 +0300

    task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -6,8 +6,50 @@
 // 32679 -> 6
 
 Console.Write("Введите число: ");
-int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()!);
+int n = 10;
+int count = 0;
+while (a > 0)
+    {
+    a = a/n; 
+    count++;
+    }
+Console.WriteLine(count);
 
-int result = a%10; // только для трехзначного числа
-Console.WriteLine(result);
+// Console.WriteLine("Третья цифра: " + a%n);
+// while ((a/(n*10)) > n/10 & (a/(n*10)<n))
+//    {
+//     if ((a/(n*10)) > n/10 & (a/(n*10)<n))    //526855 n=10  n=100 n=1000
+//     {
+//         Console.WriteLine("Третья цифра: " + a%n);
+//     }
+//     else
+//     {
 
+//     }
+//   n = n * 10;}
+ 
+
+// int sqrt_n = n*n;
+// if (a/(sqrt_n) > n/n & a/sqrt_n < n)
+// {
+//     Console.WriteLine("Третья цифра: " + a%n);
+// }
+
+// не универсальное решение
+// if (a/100 >1 & a/100 <10)
+// {
+//     Console.WriteLine("Третья цифра: " + a%10);
+// }
+// if (a/100 <1)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// if (a/100 >10 & a/100 <100)
+// {
+//     Console.WriteLine("Третья цифра: " + a%100/10);
+// }
+// if (a/100 >100 & a/100 <1000)
+// {
+//     Console.WriteLine("Третья цифра: " + a%1000/100);
+// }
