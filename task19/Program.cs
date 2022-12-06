09b77350ec761718f84cf11f7a09d515d0b51fa0
Darya <darya.kulikova88@yandex.ru>
2022-12-06 12:37:51 +0300

    task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -5,23 +5,45 @@
 // 12821 -> да
 // 23432 -> да
 
+
 Console.WriteLine("Введите пятизначное число: ");
-string number = Console.ReadLine()!;
-int i = 0;
-int n = number.Length;
-int count = 0;
-while (i < n)
+int number = int.Parse(Console.ReadLine()!);
+int a1 = (number % 100000 / 10000);
+// Console.WriteLine(a1);
+int a2 = (number % 10000 / 1000);
+// Console.WriteLine(a2);
+int a4 = (number % 100 / 10);
+// Console.WriteLine(a4);
+int a5 = (number % 10);
+// Console.WriteLine(a5);
+if (a1 == a5 & a2 == a4)
 {
-    if (number[i] == number[n - i - 1])
-    {
-        count++;
-        i++;
-        if (count == n)
-        { Console.WriteLine($"Да, число <{number}> является палиндромом"); }
-    }
-    else
-    {
-        Console.WriteLine($"Нет, число <{number}> не является палиндромом");
-        break;
-    }
+    Console.WriteLine($"Да, число <{number}> является палиндромом");
 }
+else
+{
+    Console.WriteLine($"Нет, число <{number}> не является палиндромом");
+}
+
+// Решение через строку
+// Console.WriteLine("Введите пятизначное число: ");
+// string number = Console.ReadLine()!;
+// int i = 0;
+// int n = number.Length;
+// int count = 0;
+// while (i < n)
+// {
+//     if (number[i] == number[n - i - 1])
+//     {
+//         count++;
+//         i++;
+//         if (count == n)
+//         { Console.WriteLine($"Да, число <{number}> является палиндромом"); }
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Нет, число <{number}> не является палиндромом");
+//         break;
+//     }
+// }
+
