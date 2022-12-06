4167f8ea3dc6c403f530ee47ff8b1d0d588b7e91
Darya <darya.kulikova88@yandex.ru>
2022-12-06 13:13:38 +0300

    task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -8,15 +8,7 @@
 
 Console.WriteLine("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine()!);
-int a1 = (number % 100000 / 10000);
-// Console.WriteLine(a1);
-int a2 = (number % 10000 / 1000);
-// Console.WriteLine(a2);
-int a4 = (number % 100 / 10);
-// Console.WriteLine(a4);
-int a5 = (number % 10);
-// Console.WriteLine(a5);
-if (a1 == a5 & a2 == a4)
+if (number / 1000 == number % 10 * 10 + number / 1000 % 10)
 {
     Console.WriteLine($"Да, число <{number}> является палиндромом");
 }
@@ -24,6 +16,31 @@ else
 {
     Console.WriteLine($"Нет, число <{number}> не является палиндромом");
 }
+
+// Console.WriteLine(number / 1000);
+// Console.WriteLine(number % 10 * 10);
+// Console.WriteLine(number / 1000 % 10);
+
+// Другой способ, более примитивный:
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// int a1 = (number % 100000 / 10000);
+// // Console.WriteLine(a1);
+// int a2 = (number % 10000 / 1000);
+// // Console.WriteLine(a2);
+// int a4 = (number % 100 / 10);
+// // Console.WriteLine(a4);
+// int a5 = (number % 10);
+// // Console.WriteLine(a5);
+// if (a1 == a5 & a2 == a4)
+// {
+//     Console.WriteLine($"Да, число <{number}> является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine($"Нет, число <{number}> не является палиндромом");
+// }
 
 // Решение через строку
 // Console.WriteLine("Введите пятизначное число: ");
