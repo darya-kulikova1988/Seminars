8551d14b776040bd353fb9cf7f29b4cd724d436f
Darya <darya.kulikova88@yandex.ru>
2022-12-09 13:47:17 +0300

    count char in number

---
+++ task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Количество цифр в числе = {CountChar(num)}");
+int CountChar(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
