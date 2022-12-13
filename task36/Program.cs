0cf88d15f7134176ee4384b109004358b0dd5aef
Darya <darya.kulikova88@yandex.ru>
2022-12-13 18:09:09 +0300

    сумма элементов, стоящих на нечётных позициях

--- task36/Program.cs
+++ task36/Program.cs
@@ -4,3 +4,32 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size) // int [] - описание метода
+{
+    int[] res = new int[size];  // инициализация массива 
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(-10, 11);
+    }
+    return res;
+}
+void PrintArray(int[] arr)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        Console.Write($"{arr[j]} ");
+    }
+}
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(n);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] --> Cумма элементов, стоящих на нечётных позициях = ");
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0) sum = sum + array[i];
+}
+Console.Write(sum);
