1ac91f8a9ea115e9b7ae3ba49239b8db3b628fab
Darya <darya.kulikova88@yandex.ru>
2022-12-13 18:24:26 +0300

    разница между макс и мин

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-
+// (попрактиковать next double)
 // [3 7 22 2 78] -> 76
 
 void FillArray(int[] collection) // void - метод ничего не возвращает
@@ -33,6 +33,13 @@ int[] array = new int[5];
 FillArray(array);
 PrintArray(array);
 
-
-
-Console.Write(" -> ");
+Console.Write(" -> Разница между максимальным и минимальным элементами массива = ");
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    else if (array[i] < min) min = array[i];
+}
+Console.Write(max - min);
+// Console.Write($"{max} {min}");
