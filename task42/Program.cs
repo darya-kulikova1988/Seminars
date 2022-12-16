205d2b4bdc0f6ba03ad5758f4bfd89288b1178c4
Darya <darya.kulikova88@yandex.ru>
2022-12-16 14:16:36 +0300

    перевод в двоичную систему

---
+++ task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+Console.Write("Введите число в десятичной системе счисления = ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write($"Число <{N}> в двоичной системе: <{Binary(N)}>");
+
+string Binary(int a)
+{
+    string c = string.Empty;
+    while (a > 0)
+    {
+        c = a % 2 + c;
+        a = a / 2;
+    }
+    return c;
+}
