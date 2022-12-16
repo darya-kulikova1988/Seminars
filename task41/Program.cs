44290aad3cd00c6cbc9b9bd6a38f153c0f53e76c
Darya <darya.kulikova88@yandex.ru>
2022-12-16 16:12:33 +0300

    кол-во чисел больше нуля

---
+++ task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Split() ?
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Write("Введите массив, содержащий M символов = ");
+int M = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(M);
+
+Console.WriteLine(string.Join(" ", array));
+Console.Write($"Количество чисел больше <0> = {Count(M)}");
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число: ");
+        res[i] = int.Parse(Console.ReadLine()!);
+    }
+    return res;
+}
+
+int Count(int a)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0)
+        {
+            count++;
+        }
+    }
+    return (count);
+}
