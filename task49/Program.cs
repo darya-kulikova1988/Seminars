1d2b55b7ae18a80d602c0555368a61f7540b9155
Darya <darya.kulikova88@yandex.ru>
2022-12-20 18:18:57 +0300

    четные индексы - квадраты

---
+++ task49/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NewArraySquare(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 & j % 2 == 0) { Console.Write(inArray[i, j] * inArray[i, j] + " "); }
+            else { Console.Write(inArray[i, j] + " "); }
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int colomns = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, colomns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+NewArraySquare(array);
+
+
+
+
