e646471db97e6d7dba8f5100ed5a447c9f4b17d7
Darya <darya.kulikova88@yandex.ru>
2022-12-22 23:34:27 +0300

    кол-во чисел больше нуля

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,23 +3,22 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-
 // СПОСОБ №0!!!!
-Console.Write("Введите любое количество чисел, через пробел: ");
-string array1 = Console.ReadLine()!;
-string[] arr = array1.Split(" ");
-Count(arr);
+// Console.Write("Введите любое количество чисел, через пробел: ");
+// string array1 = Console.ReadLine()!;
+// string[] arr = array1.Split(" ");
+// Count(arr);
 
-void Count(string[] m)
-{
-    int count = 0;
-    for (int j = 0; j < m.Length; j++)
-    {
-        if (int.Parse(m[j]) > 0) count++;
-        Console.Write(m[j] + " ");
-    }
-    Console.WriteLine($"--> Количество чисел больше нуля  = {count}");
-}
+// void Count(string[] m)
+// {
+//     int count = 0;
+//     for (int j = 0; j < m.Length; j++)
+//     {
+//         if (int.Parse(m[j]) > 0) count++;
+//         Console.Write(m[j] + " ");
+//     }
+//     Console.WriteLine($"--> Количество чисел больше нуля  = {count}");
+// }
 
 // СПОСОБ №1!!!!
 // Console.Write("Введите любое количество чисел, через пробел: ");
@@ -49,33 +48,33 @@ void Count(string[] m)
 
 // Способ 2!!!
 
-// Console.Write("Введите количество символов M, содержащихся в массиве: ");
-// int M = int.Parse(Console.ReadLine()!);
-// int[] array = GetArray(M);
+Console.Write("Введите количество символов M, содержащихся в массиве: ");
+int M = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(M);
 
-// Console.WriteLine(string.Join(" ", array));
-// Console.Write($"Количество чисел больше <0> = {Count(M)}");
+Console.WriteLine(string.Join(" ", array));
+Console.Write($"Количество чисел больше <0> = {Count(M)}");
 
-// int[] GetArray(int size)
-// {
-//     int[] res = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         Console.Write("Введите число: ");
-//         res[i] = int.Parse(Console.ReadLine()!);
-//     }
-//     return res;
-// }
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число: ");
+        res[i] = int.Parse(Console.ReadLine()!);
+    }
+    return res;
+}
 
-// int Count(int a)
-// {
-//     int count = 0;
-//     foreach (int el in array)
-//     {
-//         if (el > 0)
-//         {
-//             count++;
-//         }
-//     }
-//     return (count);
-// }
+int Count(int a)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0)
+        {
+            count++;
+        }
+    }
+    return (count);
+}
