e4f87e4cbdcb218501cad1d2e8246f879ee78b0c
Darya <darya.kulikova88@yandex.ru>
2022-12-22 17:35:11 +0300

    транспонирование массива

---
+++ task55/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+//  В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArrayRowsColomns(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(0); j++)
+        {
+            Console.Write(inArray[j, i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int colomns = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, colomns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+ChangeArrayRowsColomns(array);
