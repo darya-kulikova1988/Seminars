c95c57163a26d3b36a4ef69bdebbf36b6a7f6c98
Darya <darya.kulikova88@yandex.ru>
2022-12-25 10:54:09 +0300

    числа от 1 до N

---
+++ task63/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 63: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные
+// числа в промежутке от 1 до N.
+
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a < b + 1) return $"{a} " + NumbersRec(a + 1, b);
+//     else return String.Empty;
+// }
+// Console.Write("Введите значение N: ");
+// int N = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(NumbersRec(1, N));
+
+// void LineNumbers(int n)
+// {
+//     if (n != 1) LineNumbers(n - 1);
+//     Console.Write($"{n} ");
+// }
+// LineNumbers(5);
+
+string LineNumbers2(int n)
+{
+    return (n >= 2) ? LineNumbers2(n - 1) + $" {n}" : "1";
+}
+LineNumbers2(5);
