f932642c49a550fe3d10c0157b349385de6dcbbd
Darya <darya.kulikova88@yandex.ru>
2022-12-28 17:22:25 +0300

    рекурсия сумма от M до N

---
+++ task66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRec(int n, int m)
+{
+    if (n > m) return n > m ? n + SumRec(m, n - 1) : n;
+    else return m > n ? m + SumRec(m - 1, n) : m;
+}
+Console.WriteLine(SumRec(4, 8));
+
